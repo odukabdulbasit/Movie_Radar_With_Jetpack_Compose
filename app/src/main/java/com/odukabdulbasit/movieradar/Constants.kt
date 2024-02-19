1706d4837ffc7115fde1ef48c43bdfad4239a7d3
Abdülbasıt ÖDÜK <53040076+odukabdulbasit@users.noreply.github.com>
2024-02-20 00:38:37 +0300

    Update Constants.kt

--- app/src/main/java/com/odukabdulbasit/movieradar/Constants.kt
+++ app/src/main/java/com/odukabdulbasit/movieradar/Constants.kt
@@ -2,6 +2,6 @@ package com.odukabdulbasit.movieradar
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/"
-    const val apiKey = "4326b8478578831aea960d4d7bde18af"
+    const val apiKey = "YOUR_API_KEY"
 
 }
